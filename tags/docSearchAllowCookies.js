eade3a74fbed59ccb66fb01713bfca1917ffcf5b
Paul Zwicker <pazwicker@gmail.com>
2021-05-23 08:52:31 -0700

    doc search

--- tags/docSearchAllowCookies.js
+++ tags/docSearchAllowCookies.js
@@ -1,18 +1,40 @@
-function docSearchAllowCookies() {
+function getGDPRParam() {
+    try {
+      return { value: window.localStorage.getItem("gdpr"), error: null };
+    } catch(e) {
+      return { value: null, error: e };
+    }
+  }
+
+function GDPRPreference() {
     var host = document.location.hostname
     if (host == 'supergoop.com') {
         console.log('delegating to cookiebot')
         return;
     }
     try {
-        gdpr = document.location.search.split('gdpr=')[1]
+        var gdpr = getGDPRParam().value
+        if (gdpr == 'allow') {
+            dataLayer.push({
+                //event: 'allow.cookies',
+                allowCookies: gdpr
+            })
+        }
+        else if (gdpr == 'null') {
+            dataLayer.push({
+                //event: 'allow.cookies',
+                allowCookies: gdpr
+            })
+        }
+        else if (gdpr == 'deny') {
+            dataLayer.push({
+                //event: 'deny.cookies',
+                allowCookies: gdpr
+            })
+        }
+    } catch (err) { // fail "open"
         dataLayer.push({
-            event: 'allow.cookies',
-            allowCookies: gdpr
-        })
-    } catch (err) {
-        dataLayer.push({
-            event: 'allow.cookies',
+            //event: 'allow.cookies',
             allowCookies: 'allow'
         })
     }
