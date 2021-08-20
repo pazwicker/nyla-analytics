fe233881c615f08354e4378bac57cad06bf7b666
Paul Zwicker <pazwicker@gmail.com>
2021-08-19 22:02:22 -0700

    gdpr and klaviyo

---
+++ tags/klaviyo-identify.js
@@ -0,0 +1,15 @@
+<script>
+var _learnq = _learnq || [];
+_learnq.push(['identify', {
+    $email: {{DLV - Email Address}},
+    $first_name: {{JS - getVisitorFirstName}},
+    $last_name: {{JS - getVisitorLastName}},
+    $phone_number: {{JS - Phone Number}},
+    $address1: {{JS - Address Line 1}},
+    $address2: {{JS - Address Line 2}},
+    $city: {{JS - City}},
+    $region: {{JS - Region}},
+    $country: {{JS - Country}},
+    $zip: {{JS - Postal Code}}
+}]);
+</script>
