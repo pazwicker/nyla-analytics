3ca6728d09e2808520edb2f2ac4a057b882ea5f8
Paul Zwicker <pazwicker@gmail.com>
2021-08-23 15:23:42 -0700

    checkout steps

--- shopify/checkout_steps.js
+++ shopify/checkout_steps.js
@@ -4,24 +4,24 @@ switch (Shopify.Checkout.step) {
         ga('ec:setAction', 'checkout', {
             'step': 1
         });
-        ga('send', 'event', 'enhanced ecommerce', 'checkout steps', 'contact | step 1')
+        ga('send', 'event', 'enhancedecommerce', 'checkout steps', 'contact | step 1')
         break;
     case 'shipping_method':
         ga('ec:setAction', 'checkout', {
             'step': 2
         });
-        ga('send', 'event', 'enhanced ecommerce', 'checkout steps', 'shipping | step 2')
+        ga('send', 'event', 'enhancedecommerce', 'checkout steps', 'shipping | step 2')
         break;
     case 'payment_method':
         ga('ec:setAction', 'checkout', {
             'step': 3
         });
-        ga('send', 'event', 'enhanced ecommerce', 'checkout steps', 'payment | step 3')
+        ga('send', 'event', 'enhancedecommerce', 'checkout steps', 'payment | step 3')
         break;
     case 'review':
         ga('ec:setAction', 'checkout', {
             'step': 4
         });
-        ga('send', 'event', 'enhanced ecommerce', 'checkout steps', 'review | step 4')
+        ga('send', 'event', 'enhancedecommerce', 'checkout steps', 'review | step 4')
         break;
 };
